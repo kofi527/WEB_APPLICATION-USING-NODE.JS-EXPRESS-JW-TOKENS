19a19c5e2f7bc2536918d433ad7821f091f79cf1
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-02-20 02:40:22 -0600

    Connection to sql database
    Now Our code description will be centered towards database connection

--- server.js
+++ server.js
@@ -6,15 +6,62 @@ const app = express()
 //now we tell our express app to use ejs
 app.set("view engine", "ejs")
 //we tell express to use the public folder as a static folder
+app.use(express.urlencoded({extended: false}))
+
 app.use(express.static("public"))
+//this is what we call middleware
+//this is a function that runs before our routes
+app.use(function (req, res, next) {
+    res.locals.errors = []
+    next()
+})
 
 //we use get request when we are trying to visit a url
 //express is going to give us access to request and response  
 app.get("/", (req, res) => {
-    //res.send("Hello World from our cool app from kenneth")
-    // instead of using res. send lets use render
+
     res.render("homepage")
+//res.send("Hello World from our cool app from kenneth")
+// instead of using res. send lets use render
 })
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.post("/register", (req, res) => {
+   const errors = []
+
+if (typeof req.body.username !== "string") req.body.username = ""
+if (typeof req.body.password !== "string") req.body.password = ""
+
+req.body.username = req.body.username.trim()
+//provide a description to set standards for pwd evaluation
+
+    if (!req.body.username) errors.push("you must provide a username.")
+    if (req.body.username && req.body.username. length < 3) errors.push("your username must be at least 3 characters.")
+    if (req.body.username && req.body.username. length > 10) errors.push("Username cannot exceed 10 characters.")
+          //setting limitation requirements for password evaluaotr using the push  and match method.
+    if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/)) errors.push("Username can only contain alphanumeric characters.")
+    // if there are errors or no errors we render the homepage.
+    // setting up password requirements without limitations
+    if (!req.body.password) errors.push("you must provide a password.")
+        if (req.body.password && req.body.password. length < 12) errors.push("your Password must be at least 12 characters.")
+        if (req.body.password && req.body.password. length > 70) errors.push("Password cannot exceed 70 characters.")
+    
+    
+    if (errors.length) {
+        return res.render("homepage", {errors})
+    } else {
+        res.send("Thank you for filling out the form")
+    }
+// save the new user into a database 
+
+//log the user in by giving him a cookeie
+
+})
+
+
 // Lets tell our application to listen  on port 3000
 app.listen(3000)
 //NB node apps are loaded manually in memory hence with changes you have to 
